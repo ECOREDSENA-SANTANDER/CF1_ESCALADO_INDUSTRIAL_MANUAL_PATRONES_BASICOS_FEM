71da1d3f0c4e15d097510849620c20efe59632cc
Andrea Botello <andrea.botellor@misena.edu.co>
2025-09-26 12:33:42 -0500

    Video [2.0]

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -28,7 +28,7 @@
         figure
           .video
             iframe(width="560" height="315" src="https://www.youtube.com/embed/BEXPziC3NNA?si=6gTNFlm-vc_OzhRQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video. Principios de escalado y disposición de los patrones básicos
+        figcaption Video. Estructura corporal y cuadro de tallas
 
 
 
