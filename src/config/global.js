87a4cde674fc760a0c85b9e0357861eedcdc2c3e
Andrea Botello <andrea.botellor@misena.edu.co>
2025-10-08 22:17:40 -0500

    AJUSTES CFA [1.0]

--- src/config/global.js
+++ src/config/global.js
@@ -297,8 +297,8 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Erika Daniela Manrique Rueda',
-          cargo: 'Validadora y vinculadora de recursos educativos digitales',
+          nombre: 'Andrea Ardila Chaparro',
+          cargo: 'Evaluadora para contenidos inclusivos y accesibles',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
